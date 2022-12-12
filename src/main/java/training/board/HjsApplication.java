9d4649c493d86c109010fdb24cbf5bd3ed5a931d
junsang <jshwang@tlksys.com>
2022-12-12 11:49:38 +0900

    Initial commit

---
+++ src/main/java/training/board/HjsApplication.java
@@ -0,0 +1,13 @@
+package training.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HjsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/training/board/HjsApplicationTests.java
@@ -0,0 +1,13 @@
+package training.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
